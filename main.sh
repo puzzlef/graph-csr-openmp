732c8eb8ed279f50d7d7c5217b32e66d5c8951b9
Subhajit Sahu <wolfram77@gmail.com>
2023-11-17 17:22:12 +0530

    :bug: with run script

---
+++ main.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+src="graph-csr-openmp"
+out="$HOME/Logs/$src.log"
+ulimit -s unlimited
+printf "" > "$out"
+
+# Download program
+if [[ "$DOWNLOAD" != "0" ]]; then
+  rm -rf $src
+  git clone https://github.com/puzzlef/$src
+fi
+cd $src
+
+# Fixed config
+: "${TYPE:=double}"
+: "${MAX_THREADS:=64}"
+# Define macros (dont forget to add here)
+DEFINES=(""
+"-DTYPE=$TYPE"
+"-DMAX_THREADS=$MAX_THREADS"
+)
+
+# Run
+g++ ${DEFINES[*]} -std=c++17 -O3 -fopenmp main.cxx
+
+perform-all() {
+# stdbuf --output=L ./a.out ~/Data/soc-Epinions1.mtx   2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/indochina-2004.mtx  2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/uk-2002.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/arabic-2005.mtx     2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/uk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/webbase-2001.mtx    2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/it-2004.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-LiveJournal.mtx 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-Orkut.mtx       2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/asia_osm.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/europe_osm.mtx      2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/kmer_A2a.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/kmer_V1r.mtx        2>&1 | tee -a "$out"
+}
+
+perform-all
+perform-all
+perform-all
+perform-all
+perform-all
+
+# Signal completion
+curl -X POST "https://maker.ifttt.com/trigger/puzzlef/with/key/${IFTTT_KEY}?value1=$src$1"
