2cb0a51f4630b5d9d225615b453e50408da9b589
Subhajit Sahu <wolfram77@gmail.com>
2023-11-20 10:56:31 +0530

    :bug: pass around pointers, not containers

--- main.cxx
+++ main.cxx
@@ -81,9 +81,9 @@ int main(int argc, char **argv) {
   // Read MTX file body.
   symmetric = false;  // We don't want the reverse edges
   float t = measureDuration([&]() {
-    if (weighted) counts = readEdgelistFormatOmpU<NUM_PARTITIONS, true> (degrees, sources, targets, weights, data, symmetric);
-    else          counts = readEdgelistFormatOmpU<NUM_PARTITIONS, false>(degrees, sources, targets, weights, data, symmetric);
-    convertToCsrFormatOmpW<NUM_PARTITIONS>(offsets, edgeKeys, edgeValues, poffsets, pedgeKeys, pedgeValues, degrees, sources, targets, weights, counts, rows);
+    if (weighted) counts = readEdgelistFormatOmpU<NUM_PARTITIONS, true> (degrees.data(), sources.data(), targets.data(), weights.data(), data, symmetric);
+    else          counts = readEdgelistFormatOmpU<NUM_PARTITIONS, false>(degrees.data(), sources.data(), targets.data(), weights.data(), data, symmetric);
+    convertToCsrFormatOmpW<NUM_PARTITIONS>(offsets.data(), edgeKeys.data(), edgeValues.data(), poffsets.data(), pedgeKeys.data(), pedgeValues.data(), degrees.data(), sources.data(), targets.data(), weights.data(), counts, rows);
   });
   // Calculate total number of edges read.
   size_t read = 0;
